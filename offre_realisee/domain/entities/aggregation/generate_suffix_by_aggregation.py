1508a674ad2501d3d15693fd2f5f0ffdba3519cf
tmolcard <molcard.t@gmail.com>
2024-06-06 16:59:42 +0200

    FEAT: Parametrisation de la periode d'ete & fix du calendrier

--- offre_realisee/domain/entities/aggregation/generate_suffix_by_aggregation.py
+++ offre_realisee/domain/entities/aggregation/generate_suffix_by_aggregation.py
@@ -1,38 +1,51 @@
 from datetime import datetime
+from typing import Tuple
+from pytz import timezone
 
 import pandas as pd
 from workalendar.europe import France
 
-from offre_realisee.config.aggregation_config import AggregationLevel, PeriodeName, CalendrierScolaireColumns
+from offre_realisee.config.aggregation_config import (
+    DEFAULT_PERIODE_ETE, AggregationLevel, PeriodeName, CalendrierScolaireColumns
+)
+
+IDF_TIMEZONE = timezone('Europe/Paris')
 
 
-def get_period_name(date: datetime, df_calendrier_scolaire: pd.DataFrame):
+def get_period_name(date: datetime, df_calendrier_scolaire: pd.DataFrame, periode_ete: Tuple[str]):
+
+    if date.strftime("%m_%d") >= periode_ete[0] and date.strftime("%m_%d") <= periode_ete[1]:
+        return PeriodeName.ete
+
     df_filtered = df_calendrier_scolaire[df_calendrier_scolaire[
         CalendrierScolaireColumns.description].str.startswith('Vacances')]
-    row = df_filtered[(df_filtered[CalendrierScolaireColumns.start_date] <= date) & (
-            df_filtered[CalendrierScolaireColumns.end_date] >= date)]
+    row = df_filtered[
+        (pd.to_datetime(df_filtered[CalendrierScolaireColumns.start_date]) <= IDF_TIMEZONE.localize(date)) &
+        (pd.to_datetime(df_filtered[CalendrierScolaireColumns.end_date]) > IDF_TIMEZONE.localize(date))
+    ]
     if row.empty:
         return PeriodeName.plein_trafic
-    elif row[CalendrierScolaireColumns.description].values[0] == 'Vacances d\'Été':
-        return PeriodeName.ete
     else:
         return PeriodeName.vacances_scolaires
 
 
-def generate_suffix_by_aggregation(df_calendrier_scolaire: pd.DataFrame) -> dict:
+def generate_suffix_by_aggregation(
+    df_calendrier_scolaire: pd.DataFrame, periode_ete: Tuple[str] = DEFAULT_PERIODE_ETE
+) -> dict:
     calendrier = France()
 
     suffix_by_agg = {
         AggregationLevel.by_day: lambda x: x.strftime('%Y_%m_%d'),
         AggregationLevel.by_month: lambda x: x.strftime('%Y_%m'),
         AggregationLevel.by_year: lambda x: x.strftime('%Y'),
-        AggregationLevel.by_period: lambda x: x.strftime('%Y_') + get_period_name(x, df_calendrier_scolaire),
+        AggregationLevel.by_period: lambda x: x.strftime('%Y_') + get_period_name(
+            x, df_calendrier_scolaire, periode_ete),
         AggregationLevel.by_period_weekdays: lambda x: (
-            x.strftime('%Y_sunday_or_holiday_') + get_period_name(x, df_calendrier_scolaire)
+            x.strftime('%Y_sunday_or_holiday_') + get_period_name(x, df_calendrier_scolaire, periode_ete)
             if (x.weekday() == 6) or calendrier.is_holiday(x.date())
             else (
-                x.strftime('%Y_week_') + get_period_name(x, df_calendrier_scolaire) if x.weekday() < 5 else
-                x.strftime('%Y_saturday_') + get_period_name(x, df_calendrier_scolaire)
+                x.strftime('%Y_week_') + get_period_name(x, df_calendrier_scolaire, periode_ete) if x.weekday() < 5 else
+                x.strftime('%Y_saturday_') + get_period_name(x, df_calendrier_scolaire, periode_ete)
             )
         ),
         AggregationLevel.by_year_weekdays: lambda x: (
--- tests/test_unit/domain/entities/aggregation/test_generate_suffix_by_aggregation.py
+++ tests/test_unit/domain/entities/aggregation/test_generate_suffix_by_aggregation.py
@@ -3,22 +3,25 @@ from datetime import datetime
 import pandas as pd
 
 from offre_realisee.config.aggregation_config import PeriodeName
-from offre_realisee.domain.entities.aggregation.generate_suffix_by_aggregation import get_period_name
+from offre_realisee.domain.entities.aggregation.generate_suffix_by_aggregation import IDF_TIMEZONE, get_period_name
+
+
+TEST_PERIODE_ETE = ('07_01', '08_31')
 
 
 def test_get_period_name_plein_trafic():
     # Given
     date = datetime(2023, 9, 4)
     df_calendrier_scolaire = pd.DataFrame({
-        'description': ['Vacances d\'Été', 'Vacances'],
-        'start_date': [datetime(2023, 7, 1), datetime(2023, 9, 1)],
-        'end_date': [datetime(2023, 8, 31), datetime(2023, 9, 3)]
+        'description': ['Vacances'],
+        'start_date': [IDF_TIMEZONE.localize(datetime(2023, 9, 1))],
+        'end_date': [IDF_TIMEZONE.localize(datetime(2023, 9, 3))]
     })
 
     expected_result = PeriodeName.plein_trafic
 
     # When
-    result = get_period_name(date, df_calendrier_scolaire)
+    result = get_period_name(date, df_calendrier_scolaire, periode_ete=TEST_PERIODE_ETE)
 
     # Then
     assert result == expected_result
@@ -28,15 +31,15 @@ def test_get_period_name_vacances():
     # Given
     date = datetime(2023, 9, 2)
     df_calendrier_scolaire = pd.DataFrame({
-        'description': ['Vacances d\'Été', 'Vacances'],
-        'start_date': [datetime(2023, 7, 1), datetime(2023, 9, 1)],
-        'end_date': [datetime(2023, 8, 31), datetime(2023, 9, 3)]
+        'description': ['Vacances'],
+        'start_date': [IDF_TIMEZONE.localize(datetime(2023, 9, 1))],
+        'end_date': [IDF_TIMEZONE.localize(datetime(2023, 9, 3))]
     })
 
     expected_result = PeriodeName.vacances_scolaires
 
     # When
-    result = get_period_name(date, df_calendrier_scolaire)
+    result = get_period_name(date, df_calendrier_scolaire, periode_ete=TEST_PERIODE_ETE)
 
     # Then
     assert result == expected_result
@@ -46,15 +49,15 @@ def test_get_period_name_ete():
     # Given
     date = datetime(2023, 7, 4)
     df_calendrier_scolaire = pd.DataFrame({
-        'description': ['Vacances d\'Été', 'Vacances'],
-        'start_date': [datetime(2023, 7, 1), datetime(2023, 9, 1)],
-        'end_date': [datetime(2023, 8, 31), datetime(2023, 9, 3)]
+        'description': ['Vacances'],
+        'start_date': [IDF_TIMEZONE.localize(datetime(2023, 9, 1))],
+        'end_date': [IDF_TIMEZONE.localize(datetime(2023, 9, 3))]
     })
 
     expected_result = PeriodeName.ete
 
     # When
-    result = get_period_name(date, df_calendrier_scolaire)
+    result = get_period_name(date, df_calendrier_scolaire, periode_ete=TEST_PERIODE_ETE)
 
     # Then
     assert result == expected_result
