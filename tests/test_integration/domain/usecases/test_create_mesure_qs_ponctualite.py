c7e10ae367f4b5cadec781583431c89e3bb9766b
calvin.paumier <calvin.paumier-ext@iledefrance-mobilites.fr>
2024-11-05 14:48:37 +0100

    add test donnees dupliquees

--- tests/test_integration/domain/usecases/test_create_mesure_qs_ponctualite.py
+++ tests/test_integration/domain/usecases/test_create_mesure_qs_ponctualite.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from datetime import datetime
 
+import pandas as pd
 import pytest
 
 from offre_realisee.config.file_extensions import FileExtensions
@@ -17,11 +18,22 @@ TEST_DATA_PATH_CONFIG = {
     'input_path': 'input',
     'output_path': 'output',
     'input_file_name': f'offre_realisee{FileExtensions.parquet}',
+    'duplicated_input_file_name': f'offre_realisee_dupliquee{FileExtensions.parquet}',
     'calendrier_scolaire_file_name': f'calendrier_scolaire{FileExtensions.parquet}'
 }
 
 START_DATE = datetime(2023, 9, 27)
 END_DATE = datetime(2023, 9, 28)
+
+EXPECTED_FILE = os.path.join(
+    TEST_DATA_PATH, 'expected_data',
+    f"mesure_{MesureType.ponctualite}_{START_DATE.strftime('%Y_%m_%d')}{FileExtensions.csv}"
+)
+
+RESULT_PATH = os.path.join(
+    TEST_DATA_PATH, TEST_DATA_PATH_CONFIG['output_path'],
+    AggregationLevel.by_day, MesureType.ponctualite
+)
 
 
 @pytest.fixture
@@ -49,13 +61,41 @@ def test_create_mesure_qs(file_system_fixture):
     df_calendrier_scolaire = local_file_system_handler.get_calendrier_scolaire()
     suffix = generate_suffix_by_aggregation(df_calendrier_scolaire)[AggregationLevel.by_day](date)
 
-    result_clean = os.listdir(
-        os.path.join(
-            TEST_DATA_PATH, TEST_DATA_PATH_CONFIG['output_path'],
-            AggregationLevel.by_day, MesureType.ponctualite
-        )
-    )
+    result_clean = os.listdir(RESULT_PATH)
     assert f"mesure_{MesureType.ponctualite}_{suffix}" + FileExtensions.csv in result_clean
+
+    expected_result = pd.read_csv(EXPECTED_FILE)
+    result = pd.read_csv(os.path.join(RESULT_PATH, f"mesure_{MesureType.ponctualite}_{suffix}" + FileExtensions.csv))
+
+    pd.testing.assert_frame_equal(result, expected_result)
+
+
+def test_create_mesure_qs_donnee_dupliquee(file_system_fixture):
+    # Given
+    local_file_system_handler = LocalFileSystemHandler(
+        data_path=TEST_DATA_PATH,
+        input_path=TEST_DATA_PATH_CONFIG['input_path'],
+        output_path=TEST_DATA_PATH_CONFIG['output_path'],
+        input_file_name=TEST_DATA_PATH_CONFIG['duplicated_input_file_name'],
+        calendrier_scolaire_file_name=TEST_DATA_PATH_CONFIG['calendrier_scolaire_file_name'],
+    )
+
+    date = START_DATE
+
+    # When
+    create_mesure_qs_ponctualite(local_file_system_handler, date)
+
+    # Assert
+    df_calendrier_scolaire = local_file_system_handler.get_calendrier_scolaire()
+    suffix = generate_suffix_by_aggregation(df_calendrier_scolaire)[AggregationLevel.by_day](date)
+
+    result_clean = os.listdir(RESULT_PATH)
+    assert f"mesure_{MesureType.ponctualite}_{suffix}" + FileExtensions.csv in result_clean
+
+    expected_result = pd.read_csv(EXPECTED_FILE)
+    result = pd.read_csv(os.path.join(RESULT_PATH, f"mesure_{MesureType.ponctualite}_{suffix}" + FileExtensions.csv))
+
+    pd.testing.assert_frame_equal(result, expected_result)
 
 
 def test_create_mesure_qs_ponctualite_date_range(file_system_fixture):
@@ -82,10 +122,7 @@ def test_create_mesure_qs_ponctualite_date_range(file_system_fixture):
     start_date_suffix = suffix_by_agg[AggregationLevel.by_day](start_date)
     end_date_suffix = suffix_by_agg[AggregationLevel.by_day](end_date)
 
-    result_clean = os.listdir(
-        os.path.join(
-            TEST_DATA_PATH, TEST_DATA_PATH_CONFIG['output_path'], AggregationLevel.by_day, MesureType.ponctualite
-        )
-    )
+    result_clean = os.listdir(RESULT_PATH)
+
     assert f"mesure_{MesureType.ponctualite}_{start_date_suffix}" + FileExtensions.csv in result_clean
     assert f"mesure_{MesureType.ponctualite}_{end_date_suffix}" + FileExtensions.csv in result_clean
