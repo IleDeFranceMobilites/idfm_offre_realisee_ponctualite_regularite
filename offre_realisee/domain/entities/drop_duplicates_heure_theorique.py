01f3fe63678457ffc4061082c7a96d4fceb7d195
calvin.paumier <calvin.paumier-ext@iledefrance-mobilites.fr>
2024-11-08 00:04:09 +0100

    fix docstring

--- offre_realisee/domain/entities/drop_duplicates_heure_theorique.py
+++ offre_realisee/domain/entities/drop_duplicates_heure_theorique.py
@@ -4,8 +4,9 @@ from offre_realisee.config.input_config import InputColumns
 
 
 def drop_duplicates_heure_theorique(df_offre_realisee: pd.DataFrame) -> pd.DataFrame:
-    """Suppression des heures théoriques dupliquées. Si une heure théorique (pour un arrêt/sens/ligne) est lié à deux
-    heure réelle, on prend la première heure réelle.
+    """Suppression des heures théoriques dupliquées. Si une heure théorique non null (pour un arrêt/sens/ligne) est lié
+    à deux heure réelle, on prend la première heure réelle. Si la colonne heure théorique est null, dans ce cas, on
+    fait un drop duplicates classique.
 
     Cette fonction prend en entrée un DataFrame contenant les données d'offre réalisée, et renvoie un DataFrame de ces
     données sans doublons dans les heures théoriques.
