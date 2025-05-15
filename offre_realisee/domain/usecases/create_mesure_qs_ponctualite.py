78fbd714e1d79d7fda2058006913f8c6e4562aa8
tmolcard <molcard.t@gmail.com>
2025-05-15 11:50:18 +0200

    FEAT: Ajout d'une option de calcul des indicateurs par lignes

--- offre_realisee/domain/usecases/create_mesure_qs_ponctualite.py
+++ offre_realisee/domain/usecases/create_mesure_qs_ponctualite.py
@@ -5,21 +5,18 @@ import pandas as pd
 from multiprocess import Pool
 
 from offre_realisee.config.logger import logger
-from offre_realisee.config.input_config import InputColumns
 from offre_realisee.config.offre_realisee_config import MesureType
-from offre_realisee.domain.entities.drop_duplicates_heure_theorique import drop_duplicates_heure_theorique
-from offre_realisee.domain.entities.drop_stop_without_real_time import drop_stop_without_real_time
-from offre_realisee.domain.entities.add_frequency import add_frequency
-from offre_realisee.domain.entities.ponctualite.stat_compliance_score_ponctualite import (
-    stat_compliance_score_ponctualite)
-from offre_realisee.domain.entities.ponctualite.process_stop_ponctualite import process_stop_ponctualite
 from offre_realisee.domain.port.file_system_handler import FileSystemHandler
+from offre_realisee.domain.entities.ponctualite.compute_ponctualite_stat_from_dataframe import (
+    compute_ponctualite_stat_from_dataframe)
 
 
 NUMBER_OF_PARALLEL_PROCESS = 6
 
 
-def create_mesure_qs_ponctualite(file_system_handler: FileSystemHandler, date: datetime, dsp: str = ""):
+def create_mesure_qs_ponctualite(
+    file_system_handler: FileSystemHandler, date: datetime, dsp: str = "", ligne: str = ""
+) -> None:
     """Crée et sauvegarde les mesures de qualité de service de type ponctualité.
 
     Cette fonction récupère les données d'offre réalisée pour une date donnée, effectue des calculs de ponctualité pour
@@ -33,42 +30,26 @@ def create_mesure_qs_ponctualite(file_system_handler: FileSystemHandler, date: d
         Date pour laquelle les mesures de qualité de service doivent être calculées.
     dsp : str
         DSP pour laquelle les mesures de qualité de service doivent être calculées, par défaut à "".
+    ligne : str
+        Ligne pour laquelle les mesures de qualité de service doivent être calculées, par défaut à "".
     """
 
     logger.info(f'Process: {date.strftime("%Y-%m-%d")}')
 
     try:
-        df_offre_realisee = file_system_handler.get_daily_offre_realisee(date=date, dsp=dsp)
+        df_offre_realisee = file_system_handler.get_daily_offre_realisee(date=date, dsp=dsp, ligne=ligne)
     except FileNotFoundError:
-        logger.info(f'No data to process for {date.strftime("%Y-%m-%d")}, for dsp {dsp}')
+        logger.info(f'No data to process for {date.strftime("%Y-%m-%d")} with dsp: [{dsp}] and ligne: [{ligne}]')
         return
 
-    df_offre_realisee = drop_duplicates_heure_theorique(df_offre_realisee)
-
-    df_offre_realisee = drop_stop_without_real_time(df_offre_realisee)
-    df_grouped = df_offre_realisee.groupby(by=[
-        InputColumns.ligne, InputColumns.sens, InputColumns.arret
-    ])
-
-    df_concat_ponctualite = pd.DataFrame()
-    for (ligne, sens, arret), df_by_stop in df_grouped:
-        logger.debug(f'Process: ligne {ligne} - sens {sens} - arret {arret}')
-
-        df_by_stop = add_frequency(df_by_stop)
-
-        logger.debug('Ponctualite')
-        score_by_stop_ponctualite = process_stop_ponctualite(df_by_stop)
-        if not score_by_stop_ponctualite.empty:
-            df_concat_ponctualite = pd.concat([df_concat_ponctualite, score_by_stop_ponctualite], ignore_index=True)
-
-    df_stat_ponctualite = stat_compliance_score_ponctualite(df_concat_ponctualite)
+    df_stat_ponctualite = compute_ponctualite_stat_from_dataframe(df_offre_realisee)
     file_system_handler.save_daily_mesure_qs(
         df_mesure_qs=df_stat_ponctualite, date=date, dsp=dsp, mesure_type=MesureType.ponctualite
     )
 
 
 def create_mesure_qs_ponctualite_date_range(
-        file_system_handler: FileSystemHandler, date_range: tuple[datetime, datetime], dsp: str = "",
+        file_system_handler: FileSystemHandler, date_range: tuple[datetime, datetime], dsp: str = "", ligne: str = "",
         n_thread: int = NUMBER_OF_PARALLEL_PROCESS
 ) -> None:
     """Appelle la fonction create_mesure_qs_ponctualite sur une plage de date, en parallélisant les calculs.
@@ -80,7 +61,9 @@ def create_mesure_qs_ponctualite_date_range(
     date_range : datetime
         Dates de début et de fin pour laquelle les mesures de qualité de service doivent être calculées.
     dsp : str
-        DSP pour laquelle les mesures de qualité de service doivent être calculées.
+        DSP pour laquelle les mesures de qualité de service doivent être calculées, par défaut à "".
+    ligne : str
+        Ligne pour laquelle les mesures de qualité de service doivent être calculées, par défaut à "".
     n_thread: int
         Nombre de processus en parallèle.
     """
@@ -88,7 +71,7 @@ def create_mesure_qs_ponctualite_date_range(
 
     create_mesure_qs_ponctualite_partial = partial(
         create_mesure_qs_ponctualite,
-        file_system_handler=file_system_handler, dsp=dsp
+        file_system_handler=file_system_handler, dsp=dsp, ligne=ligne
     )
 
     with Pool(processes=n_thread) as pool:
--- offre_realisee/domain/usecases/create_mesure_qs_regularite.py
+++ offre_realisee/domain/usecases/create_mesure_qs_regularite.py
@@ -1,29 +1,21 @@
 from datetime import datetime
-from collections import defaultdict
 from functools import partial
 
 import pandas as pd
 from multiprocess import Pool
 
 from offre_realisee.config.logger import logger
-from offre_realisee.config.input_config import InputColumns
 from offre_realisee.config.offre_realisee_config import MesureType
-from offre_realisee.domain.entities.drop_duplicates_heure_theorique import drop_duplicates_heure_theorique
-from offre_realisee.domain.entities.drop_stop_without_real_time import drop_stop_without_real_time
-from offre_realisee.domain.entities.add_frequency import add_frequency
-from offre_realisee.domain.entities.regularite.stat_compliance_score_regularite import (
-    stat_compliance_score_regularite)
-from offre_realisee.domain.entities.regularite.process_stop_regularite import process_stop_regularite
 from offre_realisee.domain.port.file_system_handler import FileSystemHandler
-
-from offre_realisee.config.offre_realisee_config import MesureRegularite, FrequenceType
+from offre_realisee.domain.entities.regularite.compute_regularite_stat_from_dataframe import (
+    compute_regularite_stat_from_dataframe)
 
 
 NUMBER_OF_PARALLEL_PROCESS = 6
 
 
 def create_mesure_qs_regularite(
-        file_system_handler: FileSystemHandler, date: datetime, dsp: str = ""
+        file_system_handler: FileSystemHandler, date: datetime, dsp: str = "", ligne: str = ""
 ):
     """Crée et sauvegarde les mesures de qualité de service de type régularité.
 
@@ -38,49 +30,26 @@ def create_mesure_qs_regularite(
         Date pour laquelle les mesures de qualité de service doivent être calculées.
     dsp : str
         DSP pour laquelle les mesures de qualité de service doivent être calculées, par défaut à "".
+    ligne : str
+        Ligne pour laquelle les mesures de qualité de service doivent être calculées, par défaut à "".
     """
 
     logger.info(f'Process: {date.strftime("%Y-%m-%d")}')
 
     try:
-        df_offre_realisee = file_system_handler.get_daily_offre_realisee(date=date, dsp=dsp)
+        df_offre_realisee = file_system_handler.get_daily_offre_realisee(date=date, dsp=dsp, ligne=ligne)
     except FileNotFoundError:
         logger.info(f'No data to process for {date.strftime("%Y-%m-%d")}, for dsp {dsp}')
         return
 
-    df_offre_realisee = drop_duplicates_heure_theorique(df_offre_realisee)
-
-    df_offre_realisee = drop_stop_without_real_time(df_offre_realisee)
-    df_grouped = df_offre_realisee.groupby(by=[
-        InputColumns.ligne, InputColumns.sens, InputColumns.arret
-    ])
-
-    df_concat_regularite = pd.DataFrame()
-    theorique_passages_by_lignes = defaultdict(int)
-    any_high_frequency_on_lignes = defaultdict(bool)
-    for (ligne, sens, arret), df_by_stop in df_grouped:
-        logger.debug(f'Process: ligne {ligne} - sens {sens} - arret {arret}')
-
-        df_by_stop = add_frequency(df_by_stop)
-
-        logger.debug('Regularite')
-        score_by_stop_regularite = process_stop_regularite(df_by_stop)
-        if not score_by_stop_regularite.empty:
-            df_concat_regularite = pd.concat([df_concat_regularite, score_by_stop_regularite], ignore_index=True)
-            theorique_passages_by_lignes[ligne] += df_by_stop[InputColumns.heure_theorique].notna().sum()
-
-        if any(df_by_stop[MesureRegularite.frequence] == FrequenceType.haute_frequence):
-            any_high_frequency_on_lignes[ligne] = True
-
-    df_stat_regularite = stat_compliance_score_regularite(df_concat_regularite, theorique_passages_by_lignes,
-                                                          any_high_frequency_on_lignes)
+    df_stat_regularite = compute_regularite_stat_from_dataframe(df_offre_realisee)
     file_system_handler.save_daily_mesure_qs(
         df_mesure_qs=df_stat_regularite, date=date, dsp=dsp, mesure_type=MesureType.regularite
     )
 
 
 def create_mesure_qs_regularite_date_range(
-        file_system_handler: FileSystemHandler, date_range: tuple[datetime, datetime], dsp: str = "",
+        file_system_handler: FileSystemHandler, date_range: tuple[datetime, datetime], dsp: str = "", ligne: str = "",
         n_thread: int = NUMBER_OF_PARALLEL_PROCESS
 ) -> None:
     """Appelle la fonction create_mesure_qs_regularite sur une plage de date, en parallélisant les calculs.
@@ -92,7 +61,9 @@ def create_mesure_qs_regularite_date_range(
     date_range : datetime
         Dates de début et de fin pour laquelle les mesures de qualité de service doivent être calculées.
     dsp : str
-        DSP pour laquelle les mesures de qualité de service doivent être calculées.
+        DSP pour laquelle les mesures de qualité de service doivent être calculées, par défaut à "".
+    ligne : str
+        Ligne pour laquelle les mesures de qualité de service doivent être calculées, par défaut à "".
     n_thread: int
         Nombre de processus en parallèle.
     """
@@ -100,7 +71,7 @@ def create_mesure_qs_regularite_date_range(
 
     create_mesure_qs_regularite_partial = partial(
         create_mesure_qs_regularite,
-        file_system_handler=file_system_handler, dsp=dsp
+        file_system_handler=file_system_handler, dsp=dsp, ligne=ligne
     )
 
     with Pool(processes=n_thread) as pool:
